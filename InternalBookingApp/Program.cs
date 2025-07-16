b9f5b100a838fc4345c0635442fad3ffe8b45be9
Lesiba <lptjale@gmail.com>
2025-07-16 14:56:08 +0200

    Firsit Migration and Connection String

--- InternalBookingApp/Program.cs
+++ InternalBookingApp/Program.cs
@@ -1,7 +1,13 @@
+using InternalBookingApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ResourcePortal")));
 
 var app = builder.Build();
 
