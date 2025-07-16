497dc38cc64f2e759a0e5bcbabc965147dbe4166
Lesiba <lptjale@gmail.com>
2025-07-16 17:23:14 +0200

    Third migration and server-side validations

--- InternalBookingApp/Models/Entities/Resource.cs
+++ InternalBookingApp/Models/Entities/Resource.cs
@@ -1,4 +1,6 @@
-﻿namespace InternalBookingApp.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InternalBookingApp.Models.Entities
 {
     public class Resource
     {
@@ -7,6 +9,8 @@
         public required string Name { get; set; }
         public string Description { get; set; }
         public string Location { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Capacity must be a positive number.")]
         public int Capacity { get; set; }
         public bool IsAvailable { get; set; }
 
--- InternalBookingApp/Program.cs
+++ InternalBookingApp/Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Resource}/{action=Add}/{id?}");
 
 app.Run();
