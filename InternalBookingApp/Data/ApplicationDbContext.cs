a609e21dd0a422b443ab0dd9ad7d02f700dc55af
Lesiba <lptjale@gmail.com>
2025-07-16 14:23:05 +0200

    Creation of AppDbContext and Resource entity

---
+++ InternalBookingApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using InternalBookingApp.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InternalBookingApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Resource> Resources { get; set; }
+
+        //public DbSet<Booking> Bookings { get; set; }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+
+        //    modelBuilder.Entity<Booking>()
+        //        .HasOne(b => b.Resource)
+        //        .WithMany(r => r.Bookings)
+        //        .HasForeignKey(b => b.ResourceId);
+        //}
+
+    }
+}
---
+++ InternalBookingApp/Models/Entities/Resource.cs
@@ -0,0 +1,16 @@
+﻿namespace InternalBookingApp.Models.Entities
+{
+    public class Resource
+    {
+
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public int Capacity { get; set; }
+        public bool IsAvailable { get; set; }
+
+   //     public ICollection<Booking>? Bookings { get; set; }
+
+    }
+}
